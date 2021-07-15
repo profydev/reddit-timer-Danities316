4b386b10ee8ee61fb81ef572dae47ed78c9f7cd3
NUHU DANIEL <attayi_316@hotmail.com>
2021-07-15 20:25:47 +0100

    some space error corrections

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-      <Switch>
-        <Route path ="/">
-        App Placeholder
-        </Route>
+        <Switch>
+          <Route path="/">App Placeholder</Route>
 
-        <Route path ="/search">
-        Search
-        </Route>
-      </Switch>
+          <Route path="/search">Search</Route>
+        </Switch>
       </BrowserRouter>
-      
     </div>
   );
 }
--- src/globalStyles.js
+++ src/globalStyles.js
@@ -1,5 +1,5 @@
-import { createGlobalStyle } from 'styled-components';
- 
+import React from "react";
+import { createGlobalStyle } from "styled-components";
 const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
@@ -8,5 +8,4 @@ const GlobalStyle = createGlobalStyle`
     font-family: Open-Sans, Helvetica, Sans-Serif;
   }
 `;
- 
 export default GlobalStyle;
