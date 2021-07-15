eb6f234586da27f55b22acc9ed36f4d04cfbbf00
NUHU DANIEL <attayi_316@hotmail.com>
2021-07-15 14:15:12 +0100

    Addded fonts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import GlobalStyle from './globalStyles';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div>
+    <BrowserRouter>
+    <Switch>
+      <Route path='/'>
       App Placeholder
+      </Route>
+      </Switch>
+    </BrowserRouter>
     </div>
   );
 }
