289b49e64cc824c0bc59ae7648c8d5f7f5df4923
NUHU DANIEL <attayi_316@hotmail.com>
2021-07-15 21:48:27 +0100

    header

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { Normalize } from 'styled-normalize';
+import Header from './Header';
+import GlobalStyle from './GlobalStyle';
+import theme from './theme';
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Normalize />
+        <GlobalStyle />
+        <Header />
+        
         <Switch>
           <Route path='/'>App Placeholder</Route>
 
           <Route path='/search'>Search</Route>
         </Switch>
-      </BrowserRouter>
-    </div>
+      </Router>
+    </ThemeProvider>
   );
 }
 
