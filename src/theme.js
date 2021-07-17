7d0f07657a75c87bf472df5eadbb37231c9fb22a
Danities Ichaba <attayi_316@hotmail.com>
2021-07-17 19:19:13 +0100

    Update theme.js

--- src/theme.js
+++ src/theme.js
@@ -1,18 +1,19 @@
 const theme = {
-  color:{
-    text: '#93918f',
-},
-font: {
+  color: {
+    text: "#93918f",
+  },
+  font: {
     family: {
-        default: '"Montserrat", sans-serif',
-        headline: '"Bitter", serif',
+      default: '"Montserrat", sans-serif',
+      headline: '"Bitter", serif',
     },
-    size:{
-        default: '16px',
+    size: {
+      default: "16px",
     },
     lineHeight: {
-        default: 1.69,
+      default: 1.69,
     },
-},
+  },
 };
+
 export default theme;
