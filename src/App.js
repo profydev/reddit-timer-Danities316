9c4ac47cc33999bb79595619ae776205c8fb91e9
Danities Ichaba <attayi_316@hotmail.com>
2021-07-17 19:32:23 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { Normalize } from 'styled-normalize';
 import Header from './Header';
-import GlobalStyle from './GlobalStyle';
+import GlobalStyle from './globalStyle';
 import theme from './theme';
 
 function App() {
