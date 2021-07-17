fafdc4c81a5a6fa2133f093cf0f5700c77bf4100
NUHU DANIEL <attayi_316@hotmail.com>
2021-07-17 19:54:03 +0100

    correcting some errors

--- src/globalStyle.js
+++ src/globalStyle.js
@@ -1,7 +1,6 @@
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-
 @font-face {
   font-family: "Montseerat";
   font-weight: 400;
@@ -9,7 +8,6 @@ const GlobalStyle = createGlobalStyle`
   src: url("/fonts/Montserrat-Regular.woff2") format("woff2"), url("/fonts/Montserrat-Regular.woff")
         format("woff");
 }
-
 @font-face {
   font-family: "Montseerat";
   font-weight: 500;
@@ -17,7 +15,6 @@ const GlobalStyle = createGlobalStyle`
   src: url("/fonts/Montserrat-Medium.woff2") format("woff2"), url("/fonts/Montserrat-Medium.woff")
         format("woff");
 }
-
 @font-face {
   font-family: "Montseerat";
   font-weight: 700;
@@ -25,7 +22,6 @@ const GlobalStyle = createGlobalStyle`
   src: url("/fonts/Montserrat-Bold.woff2") format("woff2"), url("/fonts/Montserrat-Bold.woff")
         format("woff");
 }
-
 @font-face {
   font-family: "Montseerat";
   font-weight: 600;
@@ -33,19 +29,16 @@ const GlobalStyle = createGlobalStyle`
   src: url("/fonts/Montserrat-SemiBoldItalic.woff2") format("woff2"), url("/fonts/Montserrat-SemiBoldItalic.woff")
         format("woff");
 }
-
 @font-face {
   font-family: "Bitter";
   src: url("/fonts/Bitter-Regular.woff2") format("woff2"), url("/fonts/Montserrat-Bitter-Regular.woff")
         format("woff");
 }
-
 @font-face {
   font-family: "Bitter";
   src: url("/fonts/Bitter-Bold.woff2") format("woff2"), url("/fonts/Montserrat-Bitter-Bold.woff")
         format("woff");
 }
-
   body {
       font-family: ${(props) => props.theme.font.family.default};
       font-size: ${(props) => props.theme.font.size.default};
