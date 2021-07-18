da9d2948b61d356b791e5aab476180cc5c055509
NUHU DANIEL <attayi_316@hotmail.com>
2021-07-18 23:22:17 +0100

    ....

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -5,13 +5,13 @@ import * as S from './header.style';
 function Header() {
   return (
     <S.Container>
-       <Link to="/">
+      <Link to="/">
         <S.Logo />
       </Link>
-       <nav>
-        <S.Link to="/search/javascript">Search</S.Link>
-        <S.Link to="/#how-it-works">How it Works</S.Link>
-        <S.Link to="/#about">Abou</S.Link>
+      <nav>
+         <S.Link to="/search/javascript">Search</S.Link>
+         <S.Link to="/#how-it-works">How it Works</S.Link>
+         <S.Link to="/#about">Abou</S.Link>
       </nav>
     </S.Container>
   );
