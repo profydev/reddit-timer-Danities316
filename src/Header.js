dc1239e3262d3c090e4ca3d2ecf10aec46f1896c
Danities Ichaba <attayi_316@hotmail.com>
2021-07-18 23:44:14 +0100

    Header (#5)
    * create-app-skeleton
    
    * some space error corrections
    
    * ...
    
    * ....2
    
    * clearing some errors
    
    * header
    
    * Add Objects to GlobalStyles.js and create a src/fonts folder for fonts
    
    * correcting some errors
    
    * ....
    
    * Update globalStyle.js
    
    * Update Header.js
    
    * Update Header.js
    
    * ...
    
    * Update Header.js
    
    * Update Header.js
    
    * Update theme.js
    
    * Update Header.js
    
    * Update Header.js
    
    * Update Header.js
    
    * ...
    
    * ....
    
    * ....
    
    * ....
    
    * ....
    
    * ...
    
    * ....

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div>
+      <header>Header Placeholder</header>
+    </div>
+  );
+}
+export default Header;
---
+++ src/common/header/header.style.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+import { Link as UnstyledLink } from 'react-router-dom';
+import { ReactComponent as UnstyledLogo } from './my_logo.svg';
+
+export const Container = styled.header`
+width: 100%;
+height: 100px;
+margin: 0 auto;
+padding: 0 80px;
+display: flex;
+align-items: center;
+justify-content: space-between
+`;
+export const Logo = styled(UnstyledLogo)`
+margin-top: 8px
+`;
+
+export const Link = styled(UnstyledLink)`
+margin-left: 26;
+text-decoration: none;
+color: ${(props) => props.theme.color.midDark};
+`;
